c770348c559832e35f08e6fcb0d6b7e18eacad5a
Muhammad Fikry Rizal <fikryyoshiko70@gmail.com>
2025-08-25 22:39:11 +0700

    add allow all server

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Tambahkan ini untuk mengizinkan frontend (localhost:5173) mengakses backend
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
